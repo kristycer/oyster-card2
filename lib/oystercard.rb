c80230b9f774b58d4b789e5747765709dd49800c
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-08-10 17:51:06 +0100

    Finished chapter 7

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,13 +5,16 @@ MAXIMUM_BALANCE = 90
 
 
     def initialize
-        @balance = 11
+        @balance = 0
     end
 
     def top_up(money)
-        fail 'Maximum balance of #{maximum_balance} reached' if money + @balance > MAXIMUM_BALANCE
+        fail 'Maximum balance of 90 reached' if money + balance > MAXIMUM_BALANCE
         @balance += money
     end 
 
+    def deduct(money)
+        @balance -= money
+    end
    
 end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,16 @@ require 'oystercard'
 
 describe Oystercard do
     it 'has a balance' do
-        expect(subject.balance).to eq(11)
+        expect(subject.balance).to eq(0)
     end 
+    
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'deducts an amount from the balance' do
+      subject.top_up(5)
+      expect { subject.deduct 5}.to change { subject.balance }.by(-5)
+    end
 
 describe '#top_up' do
     
@@ -11,8 +19,8 @@ describe '#top_up' do
 
     it 'raises an error if maximum balance reached' do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up maximum_balance
-      expect { subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} reached'
+      subject.top_up (maximum_balance)
+      expect { subject.top_up 1 }.to raise_error 'Maximum balance of 90 reached'
 
         end
     end
