7e4edfd8376adf47ebecd1465eab11997831bf0a
kristyna cervenkova <kristyna275@gmail.com>
2020-08-10 16:50:26 +0200

    added balance method

---
+++ lib/oystercard.rb
@@ -0,0 +1,11 @@
+class Oystercard
+
+attr_reader :balance
+
+
+    def initialize
+        @balance = 11
+    end
+
+   
+end 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,8 @@
+require 'oystercard'
+
 describe Oystercard do
+    it 'has a balance' do
+        expect(subject.balance).to eq(11)
+    end 
 end
  
